26740e07e71040170cd5ce218baa2b4850224b55
Maxim Lesy <maxim.lesy@howest.be>
2024-12-28 10:18:44 +0100

    Red - Starting game starts with 10 pins up

--- 1-tdd/tddbowling/src/test/java/BowlingGameTests.java
+++ 1-tdd/tddbowling/src/test/java/BowlingGameTests.java
@@ -1,8 +1,17 @@
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTests {
     @Test
-    public void nothing() throws Exception {
+    public void initialisingGame_StartsWithTenPinsUp() {
+        //arrange
+        BowlingGame bowlingGame = new BowlingGame();
+        int expectedPins = 10;
 
+        //act
+        int actualPins = bowlingGame.getPins();
+
+        //assert
+        assertEquals(expectedPins, actualPins);
     }
 }
