49b5e3772cead67514c933e7dc728286161d2faf
Maxim Lesy <maxim.lesy@howest.be>
2024-12-28 11:56:50 +0100

    Green - game moves frame automatically

--- 1-tdd/tddbowling/src/main/java/be/howest/ti/sd/BowlingGame.java
+++ 1-tdd/tddbowling/src/main/java/be/howest/ti/sd/BowlingGame.java
@@ -5,6 +5,7 @@ public class BowlingGame {
     private final int TOTAL_PINS = 10;
     private int pinsUp = TOTAL_PINS;
     private int frame = 1;
+    private int rollsThisFrame = 0;
 
     public int getPinsUp() {
         return pinsUp;
@@ -19,6 +20,9 @@ public class BowlingGame {
         if (pinsToKnockDown < 0) {
             throw new RollException("Cannot knock down a negative number of pins");
         }
+
+        rollsThisFrame++;
+        if (rollsThisFrame == 2) { frame++; }
 
         pinsUp -= pinsToKnockDown;
     }
