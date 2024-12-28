ec86e843ec6b8ab2496119ad796ba43d0c564f63
Maxim Lesy <maxim.lesy@howest.be>
2024-12-28 14:09:50 +0100

    Refactor - move game verifiers to separate function

--- 1-tdd/tddbowling/src/main/java/be/howest/ti/sd/BowlingGame.java
+++ 1-tdd/tddbowling/src/main/java/be/howest/ti/sd/BowlingGame.java
@@ -22,22 +22,13 @@ public class BowlingGame {
 
     public void roll(int pinsToKnockDown) {
 
-        if (gameStatus == GameStatus.FINISHED) {
-            throw new RollException("Game is already finished");
-        }
-
-        if (pinsToKnockDown > pinsUp) {
-            throw new RollException("Cannot knock down more pins than there are up");
-        }
-
-        if (pinsToKnockDown < 0) {
-            throw new RollException("Cannot knock down a negative number of pins");
-        }
+        verifyRoll(pinsToKnockDown);
 
         updateFrame();
 
         pinsUp -= pinsToKnockDown;
     }
+
     public int score() {
         return TOTAL_PINS - pinsUp;
     }
@@ -54,6 +45,20 @@ public class BowlingGame {
             gameStatus = GameStatus.FINISHED;
         } else {
             gameStatus = GameStatus.IN_PROGRESS;
+        }
+    }
+
+    private void verifyRoll(int pinsToKnockDown) {
+        if (gameStatus == GameStatus.FINISHED) {
+            throw new RollException("Game is already finished");
+        }
+
+        if (pinsToKnockDown > pinsUp) {
+            throw new RollException("Cannot knock down more pins than there are up");
+        }
+
+        if (pinsToKnockDown < 0) {
+            throw new RollException("Cannot knock down a negative number of pins");
         }
     }
 
