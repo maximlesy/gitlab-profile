eff257ea9e7e0aef28b02f8045e7322925007821
Maxim Lesy <maxim.lesy@howest.be>
2024-12-28 11:08:50 +0100

    Red - score is returned after roll

--- 1-tdd/tddbowling/src/test/java/BowlingGameTests.java
+++ 1-tdd/tddbowling/src/test/java/BowlingGameTests.java
@@ -1,5 +1,8 @@
 import be.howest.ti.sd.BowlingGame;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTests {
@@ -25,6 +28,17 @@ public class BowlingGameTests {
         int actualPins = bowlingGame.getPinsUp();
 
         assertEquals(expectedPins, actualPins);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,1", "2,2", "3,3", "4,4", "5,5", "6,6", "7,7", "8,8", "9,9", "10,10"})
+    public void knockingDownPins_ReturnsCorrectScore(int pinsKnockedDown, int expectedScore) {
+        BowlingGame bowlingGame = new BowlingGame();
+
+        bowlingGame.roll(pinsKnockedDown);
+        int actualScore = bowlingGame.score();
+
+        assertEquals(expectedScore, actualScore);
     }
 
 }
