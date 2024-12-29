8376aec0021f2cd305c4a4e9afd3ad026c2bd70e
Maxim Lesy <maxim.lesy@howest.be>
2024-12-29 13:56:02 +0100

    Initialize assignment 2

---
+++ 2-assembler-structured-programming/T2A6_digit_swap.c
@@ -0,0 +1,43 @@
+#include "header.h"
+/*
+# Digit Swap
+Read one number (`int`) as input.
+
+If the number has exactly two digits, the program swaps both digits.
+Otherwise, the number is printed as is.
+The sign of the number is **always** preserved.
+
+**Non-functional requirement:** The input is a number, do not try to convert it to a string.
+**Hint:** The integer division does not exist in JavaScript.
+You can simulate an integer division, however, by doing a regular (decimal)
+division, and then "round the result to the floor" (naar beneden afronden).
+This can be done in JavaScript using `Math.floor(x/y)`.
+
+## Examples:
+    > 1234
+    1234
+
+    > -1234
+    -1234
+
+    > -4
+    -4
+
+    > 6
+    6
+
+    > 34
+    43
+
+    > -12
+    -21
+
+    > -70
+    -7
+*/
+int main() {	
+	end:
+	write_str("end main");
+	return 0;
+}
+
---
+++ 2-assembler-structured-programming/T6A9_fib.c
@@ -0,0 +1,35 @@
+#include "header.h"
+/*
+# Recursion
+
+Fibonacci was an Italian mathematician who lived around 1200 A.D..
+He devised a sequence of numbers, called the Fibonacci-numbers which can easily expressed
+as a "meervoudig functievoorschrift". We do not need to understand the implications, need or even mathematics.
+
+We should however be able to translate this into a recursive JavaScript function.
+
+<pre>
+          /
+          | 0, for n = 0
+          |
+fib(n) = <  1, for n = 1
+          |
+          | fib(n-1) + fib(n-2), else
+          \
+</pre>
+
+For bigger numbers, this solution is correct but too slow.
+Write a second version `fibFast` that uses a loop rather than recursion.
+**Hint:** Search the internet for help.
+**Hint:** Skip this *fast* exercise if you are running behind!
+
+
+*/
+
+int main() {
+	end:
+	write_str("end main");
+	return 0;
+}
+
+
