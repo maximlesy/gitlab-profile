328868c2bb204da85d067c5f3882956dac4c3e68
Maxim Lesy <maxim.lesy@howest.be>
2024-12-31 14:34:31 +0100

    Gilded Rose - Add initial acceptance tests

---
+++ 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRoseTests/GildedRose_AcceptanceTests.cs
@@ -0,0 +1,35 @@
+﻿using GildedRoseKata;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GildedRoseTests;
+
+public class GildedRose_AcceptanceTests
+{
+    [Fact]
+    public void UpdateQuality_Should_ReduceItemSellInTo9WhenInitializedAs10()
+    {
+        int startSellIn = 10;
+        int expectedSellIn = 9;
+        Item item = new Item { Name = "Test", Quality = 10, SellIn = startSellIn };
+        
+        GildedRose sut = new GildedRose(new List<Item> { item });
+        sut.UpdateQuality();
+
+        Assert.Equal(expectedSellIn, item.SellIn);
+    }
+
+    [Fact]
+    public void UpdateQuality_Should_ReduceItemQualityTo9WhenInitializedAs10()
+    {
+        int startQuality = 10;
+        int expectedQuality = 9;
+        Item item = new Item { Name = "Test", Quality = startQuality, SellIn = 10 };
+
+        GildedRose sut = new GildedRose(new List<Item> { item });
+        sut.UpdateQuality();
+
+        Assert.Equal(expectedQuality, item.SellIn);
+    }
+
+}
