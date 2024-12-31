80fcbbeb50494b8f653b0d8e4c5c56f153d8dfa8
Maxim Lesy <maxim.lesy@howest.be>
2024-12-31 14:52:14 +0100

    Gilded Rose - Acceptance test - Quality never negative

--- 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRoseTests/GildedRose_AcceptanceTests.cs
+++ 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRoseTests/GildedRose_AcceptanceTests.cs
@@ -7,6 +7,7 @@ namespace GildedRoseTests;
 public class GildedRose_AcceptanceTests
 {
     [Fact]
+    // At the end of each day our system lowers SellIn for every item
     public void UpdateQuality_Should_ReduceItemSellInTo9WhenInitializedAs10()
     {
         int startSellIn = 10;
@@ -20,6 +21,7 @@ public class GildedRose_AcceptanceTests
     }
 
     [Fact]
+    // At the end of each day our system lowers Quality for every item
     public void UpdateQuality_Should_ReduceItemQualityTo9WhenInitializedAs10()
     {
         int startQuality = 10;
@@ -30,6 +32,25 @@ public class GildedRose_AcceptanceTests
         sut.UpdateQuality();
 
         Assert.Equal(expectedQuality, item.SellIn);
+    }
+
+    [Fact]
+    // "The Quality of an item is never negative"
+    public void UpdateQuality_ShouldNotMakeQualityLowerThan0()
+    {
+        int expectedQuality = 0;
+        int startQuality = 10;
+        int startSellIn = 10;
+        Item item = new Item { Name = "Test", Quality = startQuality, SellIn = startSellIn };
+
+        GildedRose sut = new GildedRose(new List<Item> { item });
+
+        for (int i = 0; i < 50; i++)
+        {
+            sut.UpdateQuality();
+        }
+
+        Assert.True(item.Quality >= 0);
     }
 
 }
