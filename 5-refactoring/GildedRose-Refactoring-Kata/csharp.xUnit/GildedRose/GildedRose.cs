0df4dd8fd0559a9d1488b2519ce9af65563bbeca
Maxim Lesy <maxim.lesy@howest.be>
2025-01-01 14:06:06 +0100

    Gilded Rose - Refactor - Remove redundant if statement

--- 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRose/GildedRose.cs
+++ 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRose/GildedRose.cs
@@ -15,7 +15,6 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-
             DecreaseSellIn(Items[i]);
 
             if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
@@ -39,10 +38,7 @@ public class GildedRose
                 {
                     if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            DecreaseQuality(Items[i]);
-                        }
+                        DecreaseQuality(Items[i]);
                     }
                     else
                     {
@@ -81,7 +77,9 @@ public class GildedRose
 
     private void HandleQualityLogic(Item item)
     {
-        
+
+        if (item.Name == "Sulfuras, Hand of Ragnaros") return;
+
         if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
         {
             if (item.SellIn < 10)
