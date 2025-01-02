474d4395e7af612448899c43eac95cfa98291327
Maxim Lesy <maxim.lesy@howest.be>
2025-01-02 11:05:01 +0100

    Gilded Rose - Refactor - Clean up unused methods in GildedRose

--- 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRose/GildedRose.cs
+++ 5-refactoring/GildedRose-Refactoring-Kata/csharp.xUnit/GildedRose/GildedRose.cs
@@ -18,33 +18,7 @@ public class GildedRose
     {
         foreach (var item in Items)
         {
-            HandleQualityLogic(item);
+            itemUpdater.UpdateItem(item);
         }
-    }
-
-    private void HandleQualityLogic(Item item)
-    {
-        itemUpdater.UpdateItem(item);
-    }
-
-    private void IncreaseQuality(Item item)
-    {
-        if (item.Quality < 50)
-        {
-            item.Quality++;
-        }
-    }
-
-    private void DecreaseQuality(Item item)
-    {
-        if (item.Quality > 0)
-        {
-            item.Quality--;
-        }
-    }
-
-    private void DecreaseSellIn(Item item)
-    {
-        item.SellIn--;
     }
 }
